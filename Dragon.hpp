79a41bd21591b680f73520f9f40f980a492117fe
Daniel- <danielyg2904@gmail.com>
2019-03-25 21:55:41 +0700

    added dragon hpp that contains coordinates

---
+++ Dragon.hpp
@@ -0,0 +1,15 @@
+#ifndef DRAGON_HPP
+#define DRAGON_HPP
+
+float dragonHeadPoints[] = {
+    408.0f, 296.0f, 0.0f,
+    538.0f, 332.0f, 0.0f,
+    502.0f, 343.0f, 0.0f
+}; // Vertices
+
+float dragonHeadIndices[] = {
+    1, 2, 3
+
+}; // which indices of the points do you want to make as triangles?
+
+#endif
