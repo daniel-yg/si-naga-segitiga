778df4b1a74e1969db24ba37d7a5141cb64ca7bf
Daniel- <danielyg2904@gmail.com>
2019-03-25 22:20:06 +0700

    renamed points to vertices for consistency

--- Dragon.hpp
+++ Dragon.hpp
@@ -1,17 +1,20 @@
 #ifndef DRAGON_HPP
 #define DRAGON_HPP
 
-float dragonHeadPoints[] = {
+float dragonHeadVertices[] = {
     408.0f, 296.0f, 0.0f,
     538.0f, 332.0f, 0.0f,
     502.0f, 343.0f, 0.0f,
     624.0f, 373.0f, 0.0f,
     539.0f, 385.0f, 0.0f,
+    493.0f, 368.0f, 0.0f,
+    
 }; // Vertices
 
-float dragonHeadIndices[] = {
+unsigned int dragonHeadIndices[] = {
     0, 1, 2,
-    2, 3, 4
+    2, 3, 4,
+    2, 4, 5
 }; // which indices of the points do you want to make as triangles?
 
 #endif
