0679b3e411cbb81ea7d8078b4f0e69ad7987254e
Daniel- <danielyg2904@gmail.com>
2019-03-25 21:59:35 +0700

    Minor fix to dragon hpp example

--- Dragon.hpp
+++ Dragon.hpp
@@ -4,12 +4,14 @@
 float dragonHeadPoints[] = {
     408.0f, 296.0f, 0.0f,
     538.0f, 332.0f, 0.0f,
-    502.0f, 343.0f, 0.0f
+    502.0f, 343.0f, 0.0f,
+    624.0f, 373.0f, 0.0f,
+    539.0f, 385.0f, 0.0f,
 }; // Vertices
 
 float dragonHeadIndices[] = {
-    1, 2, 3
-
+    0, 1, 2,
+    2, 3, 4
 }; // which indices of the points do you want to make as triangles?
 
 #endif
