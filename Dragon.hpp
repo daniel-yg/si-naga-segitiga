dd1a1556bc89fcba8a4d99e21097c77c10416ef3
Daniel- <danielyg2904@gmail.com>
2019-03-26 12:46:56 +0700

    body 90% finished (needs a little bit recheck)

--- Dragon.hpp
+++ Dragon.hpp
@@ -6,15 +6,6 @@
 #define DRAGON_WINGS 2
 #define DRAGON_TAIL 3
 
-/* Vertice Dict */
-// 0 1 2 = tanduk
-// 1 3 4 = kepala
-// 2 4 5 = mulai leher (deket kepala)
-// 4 5 6 = sisa lehernya (ke deket badan)
-// 1 4 7 = rahang bawah naga
-// 4 6 8 = pelengkap leher naga
-// 2 5 9, 5 10 11, 11 12 13 = bagian belakang leher naga, 1 2 3
-
 float dragonHeadVertices[] = { 
     408.0f, 296.0f, 0.0f, // 0 ujung tanduk
     538.0f, 332.0f, 0.0f, // 1 tanduk - kanan
@@ -37,15 +28,15 @@ float dragonHeadVertices[] = {
 
 
 unsigned int dragonHeadIndices[] = {
-    0, 1, 2, 
-    1, 3, 4, 
-    2, 4, 5, 
-    4, 5, 6, 
-    1, 4, 7, 
-    4, 6, 8,
-    2, 5, 9,
-    5, 10, 11,
-    11, 12, 13
+    0, 1, 2, // tanduk
+    1, 3, 4, // kepala
+    2, 4, 5, // mulai leher (deket kepala)
+    4, 5, 6, // sisa lehernya (ke deket badan)
+    1, 4, 7, // rahang bawah naga
+    4, 6, 8, // pelengkap leher naga
+    2, 5, 9, //bagian belakang leher naga, 1
+    5, 10, 11, // bagian belakang leher naga, 2
+    11, 12, 13 // bagian belakang leher naga, 3
 
 }; // which indices of the points do you want to make as triangles?
 
@@ -54,23 +45,50 @@ float dragonBodyVertices[] = {
     580.0f, 493.0f, 0.0f, // 0 "dada" naga
     503.0f, 488.0f, 0.0f, // 1 akhir leher naga (belakang leher)
     535.0f, 574.0f, 0.0f, // 2 deket kaki depan, bagian depan
-
-    
+    521.0f, 651.0f, 0.0f, // 3 kaki depan
+    486.0f, 566.0f, 0.0f, // 4 paha naga, bagian tengah
+    503.0f, 488.0f, 0.0f, // 5 akhir leher naga (deket badan)
+    426.0f, 571.0f, 0.0f, // 6 ketek? naga
+    473.0f, 645.0f, 0.0f, // 7 kaki depan, bagian belakang
+    324.0f, 483.0f, 0.0f, // 8 punggung belakang
+    353.0f, 563.0f, 0.0f, // 9 paha belakang, bagian tengah
+    452.0f, 535.0f, 0.0f, // 10 bukan titik, di antara 1 dan 6
+    315.0f, 577.0f, 0.0f, // 11 pantat naga
+    297.0f, 601.0f, 0.0f, // 12 pangkal ekor naga (deket 11)
+    260.0f, 523.0f, 0.0f, // 13 pangkal ekor naga, bagian atas
+    345.0f, 626.0f, 0.0f, // 14 kaki naga belakang, bagian yang ketutupan huruf T kebalik (tengah)
+    404.0f, 573.0f, 0.0f, // 15 paha belakang naga, bagian depan
+    319.0f, 617.0f, 0.0f, // 16 kaki naga belakang, belakang
+    371.0f, 633.0f, 0.0f, // 17 kaki naga belakang, depan
 };
 
 unsigned int dragonBodyIndices[] = {
-    0, 1, 2
+    0, 1, 2, // dada
+    2, 3, 4, // paha depan
+    4, 5, 6, // paha belakang bagian atas
+    3, 6, 7, // paha belakang pt1
+    // 3, 4, 6, // paha belakang pt2
+    6, 8, 9, // punggung belakang, tapi bagian bawah dan belakang
+    1, 8, 10, // punggung belakang
+    8, 9, 11, // tempat paha belakang
+    8, 12, 13, // pangkal ekor naga
+    11, 14, 16, // paha belakang 1
+    14, 15, 17, // paha belakang 2
+    8, 11, 12, // penutup pangkal ekor naga
+    1, 6, 10, // penutup bagian tengah badan
+
+
 };
 
-float dragonWingsVertices = {
+float dragonWingsVertices[] = {
 
 };
 
-unsigned int dragonWingsIndices = {
+unsigned int dragonWingsIndices[] = {
 
 };
 
-float dragonTailVertices = {
+float dragonTailVertices[] = {
 
 };
 
